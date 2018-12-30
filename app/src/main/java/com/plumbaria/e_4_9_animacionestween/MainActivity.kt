0db70a859fe906c2ce991fde76ce4e5997d1d915
Quico Orozco <esi@plumbaria.com>
2018-12-30 20:16:48 +0100

    Commit terminado Ejercicio 4.9. Animaciones Tween

--- app/src/main/java/com/plumbaria/e_4_9_animacionestween/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_4_9_animacionestween/MainActivity.kt
@@ -2,11 +2,18 @@ package com.plumbaria.e_4_9_animacionestween
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var animacion : Animation = AnimationUtils.loadAnimation(this,R.anim.animacion)
+        textView.startAnimation(animacion)
+
+
     }
 }
